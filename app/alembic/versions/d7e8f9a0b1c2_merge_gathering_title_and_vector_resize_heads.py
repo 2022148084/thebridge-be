30f5c0470d12e2b5df8ec69a3cdec3c2c4fcdf6e
2022148084 <cheesetime@yonsei.ac.kr>
2026-05-17 03:26:17 +0900

    alembic errror fixed

---
+++ app/alembic/versions/d7e8f9a0b1c2_merge_gathering_title_and_vector_resize_heads.py
@@ -0,0 +1,19 @@
+"""Merge gathering title and vector resize heads
+
+Revision ID: d7e8f9a0b1c2
+Revises: b6c1d2e3f4a5, c6f2a1b3d4e5
+Create Date: 2026-05-17 00:00:00.000000
+"""
+
+revision = "d7e8f9a0b1c2"
+down_revision = ("b6c1d2e3f4a5", "c6f2a1b3d4e5")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
